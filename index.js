0328a8dc5f3aa6779a2be188728a357c18d077a8
Bunny <chanduvk133@gmail.com>
2025-08-29 19:47:04 +0530

    intial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+import express from 'express'
+import cors from "cors"
+import 'dotenv/config'
+import user from './dbmodel/user.js'
+import connectDB from './dbconfig/dbconnection.js'
+
+connectDB()
+const app = express()
+
+app.use(cors())
+app.use(express.json());
+
+app.get('/', (req, res) => { 
+    
+  res.send('Hello World')
+})
+app.get('/form', (req,res) => {
+    const person = {
+        name:"Abhishek",
+        age:22,
+        place:"Tvm",
+        email:"abhi123@gmail.com"
+    }
+    res.json({data:person})
+})
+
+app.post('/form-submit', (req,res) => {
+    
+    const {name , email } = req.body
+    user.insertOne({name ,email})
+    res.json({status:true})
+})--
+
+
+console.log("Hi")
+
+app.listen(3001,() =>{
+    console.log("Server running at port 3001")
+})
